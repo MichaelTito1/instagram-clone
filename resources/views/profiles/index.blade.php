cb2cd86b59361edb5e7ac5c341503fc53d3a11c1
MICHAEL TITO <maicheltito1@gmail.com>
2022-03-23 18:33:52 +0200

    profile policy added+fixed minor error in homectrl

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,9 +9,13 @@
         <div class="col-9 pt-5">
             <div class = "d-flex justify-content-between align-items-baseline">
                 <div><h1>{{ $user->username }}</h1></div>
-                <a href="/p/create">Add new post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
             </div>
-            <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @endcan
             <div class="d-flex">
                 <div><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="ps-2"><strong>23k</strong> followers</div>
